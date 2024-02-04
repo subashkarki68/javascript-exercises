ef2d4d72cf6afc65cd9856f5b8042bd71d9e8b19
Subash Karki <subashkarki68@gmail.com>
2024-02-04 16:41:24 +0545

    finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  if (times < 0) return "ERROR";
+  let newStr = "";
+  for (let i = 0; i < times; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
